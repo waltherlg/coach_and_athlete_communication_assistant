632cddb37d400ed6f76c267101d790e42e19dc55
Ruslan <rusishss@mail.ru>
2023-11-15 12:25:59 +0600

    created class CreateAthleteInputModel

---
+++ src/athletes/athletes.types.ts
@@ -0,0 +1,15 @@
+
+import { StringTrimNotEmpty} from "src/middlewares/validators";
+import { Length, Validate, MaxLength, IsDateString } from 'class-validator';
+
+
+export class CreateAthleteInputModel {
+    @StringTrimNotEmpty()
+    @MaxLength(50)
+    name: string;
+    @StringTrimNotEmpty()
+    @MaxLength(50)
+    surname: string;
+    @IsDateString()
+    dateOfBirth: string;
+  }
