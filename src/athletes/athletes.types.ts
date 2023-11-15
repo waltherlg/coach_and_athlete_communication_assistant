4661e4a378c0ac633b591ccde6406df761b91602
Ruslan Ismailov <rusishss@mail.ru>
2023-11-15 16:45:49 +0600

    create Athlete mongoose schema

--- src/athletes/athletes.types.ts
+++ src/athletes/athletes.types.ts
@@ -1,6 +1,8 @@
 
 import { StringTrimNotEmpty} from "src/middlewares/validators";
 import { Length, Validate, MaxLength, IsDateString } from 'class-validator';
+import { Schema, Prop, SchemaFactory } from "@nestjs/mongoose";
+import mongoose, { HydratedDocument, ObjectId, Types } from 'mongoose';
 
 
 export class CreateAthleteInputModel {
@@ -14,3 +16,21 @@ export class CreateAthleteInputModel {
     @MaxLength(50)
     dateOfBirth: string;
   }
+
+  
+
+  export type AthleteDocument = HydratedDocument<Athlete>;
+
+  @Schema()
+  export class Athlete {
+    @Prop({ required: true, type: mongoose.Schema.Types.ObjectId })
+    _id: Types.ObjectId;
+    @Prop({required: true})
+    name: string;
+    @Prop({required: true})
+    surname: string;
+    @Prop({required: true})
+    dateOfBirth: string;
+  }
+  
+  export const AthleteSchema = SchemaFactory.createForClass(Athlete);
