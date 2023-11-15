7ad6de63f1579b685ca2b7f17f374870a307a6e8
Ruslan Ismailov <rusishss@mail.ru>
2023-11-15 14:22:26 +0600

    configured AppSettings in a separate file

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { addAppSettings } from './settings';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const rawApp = await NestFactory.create(AppModule);
+  const app = addAppSettings(rawApp);
   await app.listen(3000);
 }
 bootstrap();
