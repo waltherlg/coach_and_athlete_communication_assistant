85ad4616e2d0b724d95080114f9ffc4ee1a416bd
Ruslan Ismailov <rusishss@mail.ru>
2023-11-15 15:30:10 +0600

    connected MongoDB

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,18 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SaAthletesController } from './applications/sa.athletes.controller';
 import { TrimNotEmptyValidator } from './middlewares/validators';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import * as dotenv from 'dotenv';
+dotenv.config();
+const mongoUri = process.env.MONGO_URL;
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(mongoUri, { dbName: 'coaches_and_athletes' }),
+    MongooseModule.forFeature([]),
+    ConfigModule.forRoot(),
+  ],
   controllers: [AppController,
     SaAthletesController],
   providers: [AppService,
