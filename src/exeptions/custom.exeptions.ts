e078410df0ed26813303331331a9d915dd21d691
Ruslan Ismailov <rusishss@mail.ru>
2023-11-23 10:06:03 +0600

    added CustomisableException

---
+++ src/exeptions/custom.exeptions.ts
@@ -0,0 +1,17 @@
+import { HttpException, HttpStatus } from '@nestjs/common';
+
+export class CustomisableException extends HttpException {
+    constructor(field: string, errorMessage: string, status: number) {
+      super(messageConstructor(field, errorMessage), status);
+    }
+  }
+  
+  function messageConstructor(field: string, errorMessage: string) {
+    const message = [
+      {
+        message: errorMessage,
+        field: field,
+      },
+    ];
+    return { message };
+  }
